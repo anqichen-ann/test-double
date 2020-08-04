e014c66a8ffb3873ad481254b602631e2ccbd405
Annie <anqi.chen@thoughtworks.com>
2020-08-04 19:16:48 +0800

    [anqi] commit test-double

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -1,19 +1,36 @@
 package mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
-
+    SecurityCenter securityCenter;
+    mockDoorPanel mockDoorPanel;
     @BeforeEach
     public void setUp() {
+        mockDoorPanel = new mockDoorPanel();
+        securityCenter = new SecurityCenter(mockDoorPanel);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+        assertEquals(true,mockDoorPanel.flag);
+    }
+
+    class mockDoorPanel extends DoorPanel{
+        public boolean flag = false;
+        public void close() {
+            flag = true;
+        }
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,35 @@
 package stub;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
-
+    GradeService gradeService;
+    SubGradeSystem subGradeSystem;
+    @BeforeEach
+    void setup() {
+        subGradeSystem = new SubGradeSystem();
+        gradeService = new GradeService(subGradeSystem);
+    }
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
         //Assertions.assertEquals(90.0, result);
+        double result = gradeService.calculateAverageGrades(1);
+        assertEquals(100,result);
     }
+
+    class SubGradeSystem extends GradeSystem{
+        public List<Double> gradesFor(long studentId) {
+            return Arrays.asList(100.0, 100.0, 100.0);
+        }
+    }
+
 }
